e77aedc15087b9b9841a3b12b7f68de357e5cd41
Damini Chachane <daminichachane04@gmail.com>
2025-07-16 17:44:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/layout/user-header.tsx
+++ src/components/layout/user-header.tsx
@@ -21,7 +21,6 @@ const publicationSubMenu = [
     { href: "/publications/overview", label: "Overview", icon: FileText },
     { href: "/publications/journal-support", label: "Journals Publication Support", icon: Book },
     { href: "/publications/journal-selection", label: "Journal Selection", icon: ThumbsUp },
-    { href: "/publications/book-publication", label: "Book Publication", icon: BookOpen },
     { href: "/publications/conference-proceedings", label: "Conference Proceedings", icon: Presentation },
     { href: "/publications/response-to-reviewers", label: "Response To Reviewers", icon: MessageSquare },
     { href: "/publications/peer-review", label: "Pre-Submission Peer Review", icon: Users },
