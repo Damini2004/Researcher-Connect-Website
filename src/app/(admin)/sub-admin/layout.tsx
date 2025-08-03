3c5bcaace9118665731084d9251a6182efcd9aaf
Damini Chachane <daminichachane04@gmail.com>
2025-08-03 15:25:23 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AdminHeader role=

--- src/app/(admin)/sub-admin/layout.tsx
+++ src/app/(admin)/sub-admin/layout.tsx
@@ -11,7 +11,7 @@ export default function SubAdminLayout({
     <>
       <SubAdminSidebar />
       <SidebarInset>
-        <div className="flex flex-col h-full">
+        <div className="flex flex-col h-svh">
           <AdminHeader role="sub-admin" />
           <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
             {children}
--- src/app/(admin)/super-admin/layout.tsx
+++ src/app/(admin)/super-admin/layout.tsx
@@ -14,7 +14,7 @@ export default async function SuperAdminLayout({
     <>
       <SuperAdminSidebar pendingEnquiriesCount={pendingEnquiries} />
       <SidebarInset>
-        <div className="flex flex-col h-full">
+        <div className="flex flex-col h-svh">
           <AdminHeader role="super-admin" />
           <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
             {children}
