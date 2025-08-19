702e59abb24176fb312e669a3ed447ba0a35437e
Divya Chachane <divyachachane06@gmail.com>
2025-08-19 06:57:54 +0000

    Submit Your Manuscript Join a global community of researchers. Share you

--- src/app/(public)/submit-journal/page.tsx
+++ src/app/(public)/submit-journal/page.tsx
@@ -1,3 +1,4 @@
+
 import JournalSubmissionForm from "@/components/forms/journal-submission-form";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import Image from "next/image";
